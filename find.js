5593cae46de85652c7626048dd4211b82a01c055
shirisha <shirisha@shirishas-MacBook-Air.local>
2022-07-30 21:04:26 +0530

    create find function

--- find.js
+++ find.js
@@ -1,6 +1,6 @@
 const array1 = [14,45,67,89,100]
 let newArray = array1.map(Math.sqrt);
-let array2 = array1.find(elem => elem < 50)
+let array2 = array1.find(elem => elem%5 === 0)
 return(
     console.log(array2)
 )
