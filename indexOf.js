d39536a1ee16e1eeb8dec28af3bbb5e0ee265198
shirisha <shirisha@shirishas-MacBook-Air.local>
2022-07-31 22:56:36 +0530

    create indexOf function

---
+++ indexOf.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+const integer = numbers.indexOf(3)
+const veggies = ["tomato", "carrot","onion" ]
+const snack = veggies.indexOf("carrot")
+// const snack = numbers.indexOf("carrot")
+
+return (
+    console.log(integer, snack)
+)
